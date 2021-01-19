0cc68a0a77151ae631a731292c20dac7b9ed6235
Rakesh Kumar Gupta <63153264+mrrakesh1318@users.noreply.github.com>
2021-01-19 21:34:52 +0530

    Add files via upload

--- js/temp.js
+++ js/temp.js
@@ -2,18 +2,17 @@ document.write("Temperature conversion <br>");
 
 function cel_to_fah(cel){
     let fah = ((9*cel)/5)+32;
-    document.write("Temperature in Celsius "+cel+"<br>");
-    document.write("Temperature in Fahrenheit "+fah+"<br>");
+    document.write(cel+"`C is "+fah+"`F <br>");
     document.write("<br>");
 }
 
 function fah_to_cel(fah){
     let cel = (5/9)*(fah-32);
-    document.write("Temperature in Fahrenheit "+fah+"<br>");
-    document.write("Temperature in Celsius "+cel+"<br>");
-    document.write("<br>");
+    document.write(fah+"`F is "+cel+"`C <br>");
 }
 
 cel_to_fah(37);
 fah_to_cel(212);
+
+document.write("<hr>");
 
