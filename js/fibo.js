28de5c6d6bda80ba765488cec10e40b17aa12819
Rakesh Kumar Gupta <63153264+mrrakesh1318@users.noreply.github.com>
2021-01-19 20:57:03 +0530

    Add files via upload

---
+++ js/fibo.js
@@ -0,0 +1,13 @@
+document.write("Fibonacci Series " + "<br>");
+
+function Fibonacci(n){
+    var a = 0, b = 1, next = 0;
+    for (i = 1; i <= n; i++){
+        document.write(a+",");
+        next = a+b;
+        a = b;
+        b = next;
+    } 
+}
+
+Fibonacci(8);
