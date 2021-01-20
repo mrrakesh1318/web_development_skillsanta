2e4469cb2d32cdacb4fdfb079fb90f23ad18c140
Rakesh Kumar Gupta <63153264+mrrakesh1318@users.noreply.github.com>
2021-01-20 21:54:01 +0530

    Add files via upload

--- js/hide_email.js
+++ js/hide_email.js
@@ -1,8 +1,10 @@
-//var email ="eddygrant@example.com";
-
 function protect_email(s)
-{    
-    return (document.write(s.strike()))
+{
+    s1 = s.split("@");
+    part = s1[0].substring(0,s1[0].length/2)+'...';
+    return (document.write(part+"@"+s1[1]));
 }
 
 protect_email("eddygrant@example.com");
+
+document.write("<hr>")
